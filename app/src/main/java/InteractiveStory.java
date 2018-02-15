f7518d54c409013e6dcefe723fe5d32a6ee1eeb3
Ali Rahmat Ismail <chiriku.ali.rhmat@gmail.com>
2018-02-15 09:58:46 +0700

    add InteractiveStory class
    class who extends Application to run this class on entire application , and add method to make aplication orientation potrait only

---
+++ app/src/main/java/InteractiveStory.java
@@ -0,0 +1,52 @@
+import android.app.Activity;
+import android.app.Application;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+/**
+ * Created by chirikualii on 15/02/18.
+ */
+
+public class InteractiveStory extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+        });
+    }
+}
